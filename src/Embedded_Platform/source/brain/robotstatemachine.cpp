dbcf0da95b48e97a1181c9a3440269a7d85b9bbf
Vũ Minh Khôi <khoi.vuminh241207@hcmut.edu.vn>
2025-12-26 13:14:17 +0700

    Fix %d của timedecisecond thành %hhu vì tràn bộ nhớ. Thêm 1 phần trả về speed để tiện debug.

--- src/Embedded_Platform/source/brain/robotstatemachine.cpp
+++ src/Embedded_Platform/source/brain/robotstatemachine.cpp
@@ -298,7 +298,7 @@ namespace brain{
         int speed, steer;
         uint8_t time_deciseconds;
 
-        uint8_t parsed = sscanf(message, "%d;%d;%d", &speed, &steer, &time_deciseconds);
+        uint8_t parsed = sscanf(message, "%d;%d;%hhu", &speed, &steer, &time_deciseconds);
 
         if(uint8_globalsV_value_of_kl != 30){
             sprintf(response,"kl 30 is required!!");
@@ -318,7 +318,7 @@ namespace brain{
             m_steeringControl.setAngle(steer);
             m_speedingControl.setSpeed(speed);
 
-            sprintf(response, "%d;%d", m_speedingControl.pwm_value, m_steeringControl.pwm_value);
+            sprintf(response, "%d;%d;%d", m_speedingControl.pwm_value, m_steeringControl.pwm_value, speed);
 
             m_calibON = true;
         }
