20993dc4ac1d9146eac065eb6b867035a4395b67
KhoiVuMinh2412 <khoi.vuminh241207@hcmut.edu.vn>
2026-01-06 22:48:29 +0700

    deleted the built files to prevent directory errors

--- src/Brain_ROS2/build/bfmc_hardware/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/kyrgios/Documents/bfmc_ws/install/bfmc_interfaces/share/bfmc_interfaces/package.sh"
--- src/Brain_ROS2/build/bfmc_interfaces/bfmc_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyrgios/Documents/bfmc_ws/build/bfmc_interfaces/bfmc_interfaces__py
-# Build directory: /home/kyrgios/Documents/bfmc_ws/build/bfmc_interfaces/bfmc_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
