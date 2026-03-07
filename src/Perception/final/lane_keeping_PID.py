d4549e38210c0a0a0b2f30c39c6348aa41bd60e0
bui-thien-nghia <nghiabui0924@gmail.com>
2026-03-07 09:46:50 +0700

    Minor fix to Controller

--- src/Perception/final/lane_keeping_PID.py
+++ src/Perception/final/lane_keeping_PID.py
@@ -40,7 +40,7 @@ class LaneController:
         if target_poly is None:
             self.no_lane_counter += 1
             if self.no_lane_counter < self.PATIENCE_LIMIT:
-                return np.rad2deg(self.prev_steer), self.min_speed, "F" #free
+                return np.rad2deg(self.prev_steer), self.min_speed, "F", None #free
             else:
                 return 0.0, 0.0, 0.0, "N" #No_lane
         
